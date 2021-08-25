f4c474483db9877fa9893cb06886167d5ecab758
Ryan959 <79676270+Ryan959@users.noreply.github.com>
2021-08-24 19:54:07 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,30 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyDkvJXD2NUrCSMeEhPdtCdfwQscjlG_z6M",
+      authDomain: "project-94-587d2.firebaseapp.com",
+      databaseURL: "https://project-94-587d2-default-rtdb.firebaseio.com",
+      projectId: "project-94-587d2",
+      storageBucket: "project-94-587d2.appspot.com",
+      messagingSenderId: "569665836440",
+      appId: "1:569665836440:web:40fdc5d50a1492a2ce07ed",
+      measurementId: "G-B3E6KEJ9MM"
+    };
+    firebase.initializeApp(firebaseConfig);
+    user_name = localStorage.getItem("user_name");
+    
+    document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+    
+    function addRoom(){
+          room_name = document.getElementById("room_name").value 
+
+          firebase.database().ref("/").child(room_name).update({
+                purpose : "adding room name"
+          });
+    }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
